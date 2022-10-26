280cfc564b41706396848c8117425cd89ee66721
Marco Morozzi <marco.morozzi2002@gmail.com>
2022-10-26 21:51:16 +0200

    manutenzione

--- components/module/PrenotationButton.tsx
+++ components/module/PrenotationButton.tsx
@@ -7,86 +7,94 @@ import { Size } from "../../types/components/Utils";
 import Button, { ButtonProps, ButtonVariants } from "../base/Button";
 import useAxiosAuth from "../../hooks/useAuthAxios";
 
-type BookingState = { booking: boolean; isSuccess: boolean; isLoading: boolean };
-const defaultbookingState = { booking: false, isSuccess: false, isLoading: false };
+// type BookingState = { booking: boolean; isSuccess: boolean; isLoading: boolean };
+// const defaultbookingState = { booking: false, isSuccess: false, isLoading: false };
+
+// export default function PrenotationButton({ event }: { event: Event }) {
+//   const { auth, setAuth } = useAuth() as AuthContext;
+//   const axios = useAxiosAuth({});
+
+//   const router = useRouter();
+//   const referral = router.query.ref;
+
+//   const canBook =
+//     new Date(event.open_date).getTime() < new Date().getTime() &&
+//     new Date().getTime() < new Date(event.close_date).getTime();
+
+//   const [{ booking, isSuccess, isLoading }, setBooking] =
+//     React.useState<BookingState>(defaultbookingState);
+
+//   React.useEffect(() => {
+//     if (auth.accessToken) {
+//       setBooking((old) => ({ ...old, isLoading: true }));
+//       axios
+//         .get("user/booking", { headers: { Authorization: `Bearer ${auth.accessToken}` } })
+//         .then((response) => {
+//           if (!response.data || response.data === " ") {
+//             console.log("no booking");
+//             setBooking({ booking: false, isSuccess: true, isLoading: false });
+//           } else response.data;
+//           const data = response?.data as Booking;
+//           const hasBooking = data.event.id === event.id;
+//           setBooking({ booking: hasBooking, isSuccess: true, isLoading: false });
+//         })
+//         .catch(console.log);
+//     }
+//   }, [auth, axios, event.id]);
+
+//   const buttonClickHandler = () => {
+//     if (auth.accessToken) {
+//       if (isSuccess && booking) router.push("/user");
+//       else if (canBook) {
+//         setBooking((old) => ({ ...old, isLoading: true }));
+//         axios
+//           .post(
+//             `event/${event.id}/booking`,
+//             {
+//               referral_link: referral ? referral : undefined,
+//             },
+//             {
+//               headers: { Authorization: `Bearer ${auth.accessToken}` },
+//             }
+//           )
+//           .then(() => {
+//             setBooking({ booking: true, isSuccess: true, isLoading: false });
+//             router.push("/user");
+//           })
+//           .catch(console.log);
+//       }
+//     }
+//   };
+
+//   let buttonText = canBook ? "Accedi per prenotare" : "Prenotazioni chiuse";
+//   if (auth.accessToken) {
+//     if (!isSuccess) buttonText = "...";
+//     if (isSuccess && isLoading) buttonText = "Prenotazione in corso...";
+//     if (isSuccess && booking) buttonText = "Visualizza prenotazione";
+//     else buttonText = canBook ? "Prenota" : "Prenotazioni chiuse";
+//   }
+
+//   console.log(canBook);
+//   return (
+//     <Button
+//       disabled={
+//         !auth.accessToken ||
+//         (!!auth.accessToken && !isSuccess) ||
+//         (!!auth.accessToken && isSuccess && !booking && !canBook)
+//       }
+//       onClick={buttonClickHandler}
+//       size={Size.large}
+//       variant={ButtonVariants.contained}
+//     >
+//       {buttonText}
+//     </Button>
+//   );
+// }
 
 export default function PrenotationButton({ event }: { event: Event }) {
-  const { auth, setAuth } = useAuth() as AuthContext;
-  const axios = useAxiosAuth({});
-
-  const router = useRouter();
-  const referral = router.query.ref;
-
-  const canBook =
-    new Date(event.open_date).getTime() < new Date().getTime() &&
-    new Date().getTime() < new Date(event.close_date).getTime();
-
-  const [{ booking, isSuccess, isLoading }, setBooking] =
-    React.useState<BookingState>(defaultbookingState);
-
-  React.useEffect(() => {
-    if (auth.accessToken) {
-      setBooking((old) => ({ ...old, isLoading: true }));
-      axios
-        .get("user/booking", { headers: { Authorization: `Bearer ${auth.accessToken}` } })
-        .then((response) => {
-          if (!response.data || response.data === " ") {
-            console.log("no booking");
-            setBooking({ booking: false, isSuccess: true, isLoading: false });
-          } else response.data;
-          const data = response?.data as Booking;
-          const hasBooking = data.event.id === event.id;
-          setBooking({ booking: hasBooking, isSuccess: true, isLoading: false });
-        })
-        .catch(console.log);
-    }
-  }, [auth, axios, event.id]);
-
-  const buttonClickHandler = () => {
-    if (auth.accessToken) {
-      if (isSuccess && booking) router.push("/user");
-      else if (canBook) {
-        setBooking((old) => ({ ...old, isLoading: true }));
-        axios
-          .post(
-            `event/${event.id}/booking`,
-            {
-              referral_link: referral ? referral : undefined,
-            },
-            {
-              headers: { Authorization: `Bearer ${auth.accessToken}` },
-            }
-          )
-          .then(() => {
-            setBooking({ booking: true, isSuccess: true, isLoading: false });
-            router.push("/user");
-          })
-          .catch(console.log);
-      }
-    }
-  };
-
-  let buttonText = canBook ? "Accedi per prenotare" : "Prenotazioni chiuse";
-  if (auth.accessToken) {
-    if (!isSuccess) buttonText = "...";
-    if (isSuccess && isLoading) buttonText = "Prenotazione in corso...";
-    if (isSuccess && booking) buttonText = "Visualizza prenotazione";
-    else buttonText = canBook ? "Prenota" : "Prenotazioni chiuse";
-  }
-
-  console.log(canBook);
   return (
-    <Button
-      disabled={
-        !auth.accessToken ||
-        (!!auth.accessToken && !isSuccess) ||
-        (!!auth.accessToken && isSuccess && !booking && !canBook)
-      }
-      onClick={buttonClickHandler}
-      size={Size.large}
-      variant={ButtonVariants.contained}
-    >
-      {buttonText}
+    <Button disabled={true} size={Size.large} variant={ButtonVariants.contained}>
+      Sito in manutenzione
     </Button>
   );
 }
--- components/template/Appbar.tsx
+++ components/template/Appbar.tsx
@@ -5,32 +5,32 @@ import Button, { ButtonVariants } from "../base/Button";
 import styles from "./Appbar.module.scss";
 
 const Appbar = (): JSX.Element => {
-  const {
-    auth: { accessToken },
-    setAuth,
-  } = useAuth() as AuthContext;
+  // const {
+  //   auth: { accessToken },
+  //   setAuth,
+  // } = useAuth() as AuthContext;
 
-  const router = useRouter();
-  const axios = useAxiosAuth({});
+  // const router = useRouter();
+  // const axios = useAxiosAuth({});
 
-  const buttonClickHandler = () => {
-    if (!accessToken) router.push(`/login?next=${router.asPath}`);
-    else
-      axios
-        .get("logout", { headers: { Authorization: `Bearer ${accessToken}` } })
-        .then(() => setAuth((old) => ({ ...old, accessToken: null })))
-        .catch(console.log);
-  };
+  // const buttonClickHandler = () => {
+  //   if (!accessToken) router.push(`/login?next=${router.asPath}`);
+  //   else
+  //     axios
+  //       .get("logout", { headers: { Authorization: `Bearer ${accessToken}` } })
+  //       .then(() => setAuth((old) => ({ ...old, accessToken: null })))
+  //       .catch(console.log);
+  // };
 
   return (
     <div className={styles.container}>
       <div className={styles.bar}>
         <h1 className="logo">Where is party?</h1>
-        <div className={styles.button_container}>
+        {/* <div className={styles.button_container}>
           <Button variant={ButtonVariants.text} onClick={buttonClickHandler}>
             {accessToken ? "Esci" : "Accedi"}
           </Button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
