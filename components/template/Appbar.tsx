23ff1ed8490dc4a0a32918d74cafc30dafa9de0e
Marco Morozzi <marco.morozzi2002@gmail.com>
2022-10-27 12:52:49 +0200

    back online

--- components/template/Appbar.tsx
+++ components/template/Appbar.tsx
@@ -4,35 +4,31 @@ import useAxiosAuth from "../../hooks/useAuthAxios";
 import Button, { ButtonVariants } from "../base/Button";
 import styles from "./Appbar.module.scss";
 
-const Appbar = (): JSX.Element => {
-  // const {
-  //   auth: { accessToken },
-  //   setAuth,
-  // } = useAuth() as AuthContext;
+export default function Appbar() {
+  const { auth, setAuth } = useAuth() as AuthContext;
 
-  // const router = useRouter();
-  // const axios = useAxiosAuth({});
+  const router = useRouter();
+  const axios = useAxiosAuth({});
 
-  // const buttonClickHandler = () => {
-  //   if (!accessToken) router.push(`/login?next=${router.asPath}`);
-  //   else
-  //     axios
-  //       .get("logout", { headers: { Authorization: `Bearer ${accessToken}` } })
-  //       .then(() => setAuth((old) => ({ ...old, accessToken: null })))
-  //       .catch(console.log);
-  // };
+  const buttonClickHandler = () => {
+    if (!auth.accessToken) router.push(`/login?next=${router.asPath}`);
+    else
+      axios
+        .get("logout", { headers: { Authorization: `Bearer ${auth.accessToken}` } })
+        .then(() => setAuth((old) => ({ ...old, accessToken: null })))
+        .catch(console.log);
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.bar}>
         <h1 className="logo">Where is party?</h1>
-        {/* <div className={styles.button_container}>
+        <div className={styles.button_container}>
           <Button variant={ButtonVariants.text} onClick={buttonClickHandler}>
-            {accessToken ? "Esci" : "Accedi"}
+            {auth.accessToken ? "Esci" : "Accedi"}
           </Button>
-        </div> */}
+        </div>
       </div>
     </div>
   );
-};
-export default Appbar;
+}
